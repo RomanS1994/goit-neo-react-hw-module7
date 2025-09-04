76128c83282eb8ca073818a9afdd0ddebc9bed70
Roman Stryzhka <rstrizhko1994@gmail.com>
2025-09-04 21:23:00 +0200

    fix configureStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,31 +3,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import contactsSliceReducer from './contactsSlice.js';
 import filtersSliceReducer from './filtersSlice.js';
 
-configureStore({
-  reducer: {
-    contacts: contactsSliceReducer,
-    filters: filtersSliceReducer,
-  },
-});
-
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-
 export const store = configureStore({
   reducer: {
     contacts: contactsSliceReducer,
     filters: filtersSliceReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
